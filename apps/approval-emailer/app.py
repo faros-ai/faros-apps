0591a5dfda5125b68eb23c13b15436228bdb1596
Chris Wu <chris@faros.ai>
2020-10-21 16:28:49 -0700

    initial app for sending approval emails

---
+++ apps/approval-emailer/app.py
@@ -0,0 +1,34 @@
+from faros.client import FarosClient
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+
+
+def lambda_handler(event, context):
+    client = FarosClient.from_event(event)
+    params = event['params']
+    recipient = params['recipient']
+
+    file_loader = FileSystemLoader(os.path.dirname(__file__))
+    env = Environment(loader=file_loader)
+    template = env.get_template('email.html')
+    html = template.render()
+
+    subject = 'Faros AI notification'
+
+    query = '''mutation($to: [String!]!, $subject: String!, $htmlBody: String!) {
+        faros_send_email(
+            to: $to
+            subject: $subject
+            textBody: "This report can only be seen in HTML enabled email clients"
+            htmlBody: $htmlBody
+        )
+    }'''
+    variables = {
+        "to": [recipient],
+        "subject": subject,
+        "htmlBody": html
+    }
+
+    response = client.graphql_execute(query, variables)
+    return response
